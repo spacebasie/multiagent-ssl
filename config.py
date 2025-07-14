d544b4e60b6e291cc6ac61b3d76a7e0189d7137f
Stefanos Agamemnon Frilingos <frilis@umich.edu>
2025-07-14 17:23:53 +0100

    mini fix

--- config.py
+++ config.py
@@ -26,6 +26,11 @@ BATCH_SIZE = 256
 NUM_WORKERS = 4
 LEARNING_RATE = 0.01 # For centralized runs try 0.05
 
+# --- Model Parameters ---
+PROJECTION_INPUT_DIM = 512
+PROJECTION_HIDDEN_DIM = 2048
+PROJECTION_OUTPUT_DIM = 2048
+
 # --- VICReg Loss Hyperparameters ---
 LAMBDA = 32.0 # Your fine-tuned value
 MU = 32.0   # Your fine-tuned value
