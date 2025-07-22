292ddc1a8f131b1809e32b6f434af09e80c219a0
Stefanos Agamemnon Frilingos <frilis@umich.edu>
2025-07-22 16:25:01 +0100

    label skew check

--- pbs.sh
+++ pbs.sh
@@ -37,7 +37,7 @@ source ~/venv/venv25/bin/activate
 echo "Starting Python script..."
 python main.py \
     --mode 'decentralized' \
-    --heterogeneity_type 'domain_shift' \
+    --heterogeneity_type 'label_skew' \
     --topology 'ring' \
     --num_agents 5 \
     --comm_rounds 10 \
