7d90e89c44a575bd19d1385e7306b3184a1bcff8
Stefanos Agamemnon Frilingos <frilis@umich.edu>
2025-08-09 15:35:15 +0100

    Update pbs.sh

--- pbs.sh
+++ pbs.sh
@@ -37,7 +37,7 @@ source ~/venv/venv25/bin/activate
 echo "Starting Python script..."
 python main.py \
     --mode 'decentralized' \
-    --dataset 'office_random' \
+    --dataset 'office_home' \
     --topology 'random' \
     --heterogeneity_type 'office_random' \
     --num_agents 5 \
