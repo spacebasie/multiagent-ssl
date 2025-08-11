0755d2e3cd96ed467363efaf0a592d6ef669cfaf
Stefanos Agamemnon Frilingos <frilis@umich.edu>
2025-08-11 09:36:23 +0100

    Update pbs.sh

--- pbs.sh
+++ pbs.sh
@@ -42,9 +42,9 @@ python main.py \
     --heterogeneity_type 'office_random' \
     --num_agents 5 \
     --comm_rounds 100 \
-    --local_epochs 1 \
+    --local_epochs 2 \
     --alpha 100 \
     --eval_every 5 \
-    --num_classes 20 \
+    --num_classes 10 \
 
 echo "Job finished at $(date)"
