e5ccc8926b3abbc13fa50e8578454cad753b653f
Stefanos Agamemnon Frilingos <frilis@umich.edu>
2025-08-25 14:27:10 +0100

    Update pbs.sh

--- pbs.sh
+++ pbs.sh
@@ -38,7 +38,7 @@ echo "Starting Python script..."
 python main.py \
     --mode 'decentralized' \
     --dataset 'cifar10' \
-    --topology 'disconnected' \
+    --topology 'fully_connected' \
     --heterogeneity_type 'label_skew_personalized' \
     --num_agents 15 \
     --eval_every 5 \
