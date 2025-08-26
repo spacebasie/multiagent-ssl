bd1282122c86e47bd1dedc95b25038ff53bf454c
Stefanos Agamemnon Frilingos <frilis@umich.edu>
2025-08-26 14:06:51 +0100

    Update pbs.sh

--- pbs.sh
+++ pbs.sh
@@ -39,6 +39,7 @@ python main.py \
     --mode 'decentralized' \
     --dataset 'cifar10' \
     --heterogeneity_type 'label_skew_personalized' \
+    --topology 'fully_connected' \
     --num_agents 5 \
     --alpha 0.5 \
     --eval_every 5 \
